a59402e755ccb2a34a0f6dee861c1e3201281d68
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-02-06 23:58:10 +0900

    Focus: Create Preparation screen & Medal enum

--- ReadyViewController.swift
+++ ReadyViewController.swift
@@ -7,7 +7,72 @@
 
 import UIKit
 
+extension Medal {
+    init(by duration: TimeInterval) {
+        if duration < 30 {
+            self = .bronze
+        } else if 30 <= duration && duration < 50 {
+            self = .sliver
+        } else {
+            self = .gold
+        }
+    }
+    
+    var icon: UIImage {
+        switch self {
+        case .gold:
+            return UIImage(named: "gold")!
+        case .sliver:
+            return UIImage(named: "sliver")!
+        case .bronze:
+            return UIImage(named: "bronze")!
+        }
+    }
+    
+    var color: UIColor {
+        switch self {
+        case .gold:
+            return UIColor(red: 255 / 255, green: 208 / 255, blue: 81 / 255, alpha: 1)
+        case .sliver:
+            return UIColor(red: 177 / 255, green: 161 / 255, blue: 182 / 255, alpha: 1)
+        case .bronze:
+            return UIColor(red: 225 / 255, green: 179 / 255, blue: 170 / 255, alpha: 1)
+        }
+    }
+}
+
+extension UIView {
+    func animateHighlight() {
+        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseIn, animations: {
+            self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
+                self.transform = .identity
+            })
+        })
+    }
+}
+
+extension UIView {
+    func makeRound(masksToBounds: Bool = false) {
+        self.layer.cornerRadius = self.frame.height / 2
+        self.layer.cornerCurve = .continuous
+        self.layer.masksToBounds = masksToBounds
+    }
+}
+
+extension UIView {
+    func addShadow(with color: UIColor) {
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowRadius = 5
+    }
+}
+
 class ReadyViewController: UIViewController {
+    private var currentMedal = Medal.bronze
+    private let range: (from: Float, to: Float) = (from: 20, to: 60)
+    
     @IBOutlet weak private var imageView: UIImageView!
     @IBOutlet weak private var startButton: UIButton!
     @IBOutlet weak private var durationLabel: UILabel!
@@ -15,6 +80,14 @@ class ReadyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        slider.value = 0
+        slider.minimumValue = range.from
+        slider.maximumValue = range.to
+        
+        startButton.makeRound()
+        
+        self.durationDidChange()
     }
 
     @IBAction func presentMedalList() {
@@ -26,7 +99,21 @@ class ReadyViewController: UIViewController {
     }
     
     @IBAction func durationDidChange() {
-        print(slider.value)
+        let medal = Medal(by: TimeInterval(slider.value))
+        let medalDidChange = medal != currentMedal
+        
+        if medalDidChange {
+            imageView.animateHighlight()
+        }
+        
+        durationLabel.text = "\(Int(slider.value)) minutes"
+        startButton.backgroundColor = medal.color
+        startButton.addShadow(with: currentMedal.color)
+        
+        imageView.image = medal.icon
+        slider.tintColor = medal.color
+        
+        self.currentMedal = medal
     }
 }
 
